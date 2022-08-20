0cfdc548ec194f86b0592b54b8dfa3685da0f6af
U-jjin <U-jjin@githum.com>
2022-08-21 01:38:13 +0900

    레포지토리 save findone, findAll 기본 로직 구현 완료

--- src/main/java/com/kurly/kurlyproject/repository/ItemRepository.java
+++ src/main/java/com/kurly/kurlyproject/repository/ItemRepository.java
@@ -1,7 +1,9 @@
 package com.kurly.kurlyproject.repository;
 
 
-import com.kurly.kurlyproject.domain.item.Item;
+import com.kurly.kurlyproject.domain.Item;
+import com.kurly.kurlyproject.dto.ItemDTO;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,10 +11,10 @@ import javax.persistence.EntityManager;
 
 import java.util.*;
 
+
 @Repository
 @RequiredArgsConstructor
 public class ItemRepository {
-
     private final EntityManager em;
 
     public void save(Item item){
@@ -20,12 +22,25 @@ public class ItemRepository {
     }
 
     public Item findOne(Long id){
-        return em.find(Item.class,id);
+        return em.find(Item.class, id);
     }
 
     public List<Item>findAll(){
-        return em.createQuery("select i from Item i",Item.class).getResultList();
+        return  em.createQuery("select i from Item i",Item.class).getResultList();
     }
+
+
+//    public ItemDTO findOne(Long id){
+//        return  DtoConvertor.convertToDto(em.find(Item.class, id));
+//    }
+//
+//    public List<ItemDTO>findAll(){
+//        List<ItemDTO> itemDTOS =new ArrayList<>();
+//        for(Item item: em.createQuery("select i from Item i",Item.class).getResultList()){
+//            itemDTOS.add(DtoConvertor.convertToDto(item));
+//        }
+//        return itemDTOS;
+//    }
 
 
 }
