31af90764035234b9efa26921211349fce082a32
otterp012 <otterp012@gmail.com>
2022-08-24 14:53:00 +0900

    remove

--- Front-end/admin/lib/lib.ts
+++
@@ -1,7 +0,0 @@
-export const fetcher = async (url: string, query: string, slug?: string) => {
-  const URL = `${url}${query}${slug ? slug : ""}`;
-  const response = await fetch(URL);
-  const data = await response.json();
-
-  return data;
-};
--- Front-end/review/components/UI/divider.tsx
+++
@@ -1,7 +0,0 @@
-const Divider = () => {
-  return (
-    <span className='w-[90%] h-1 border-b mt-0 mx-auto border-b-lightGray' />
-  );
-};
-
-export default Divider;
